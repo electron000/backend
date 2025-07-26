83b564b827b30a901043a5b61260f0ec951aba3e
electron000 <ajarunchang@gmail.com>
2025-07-26 22:30:19 +0530

    many improvements

--- app.py
+++ app.py
@@ -30,9 +30,9 @@ from reportlab.lib.enums import TA_CENTER
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "https://mainsys.vercel.app"}})
 
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-ADMIN_DEFAULT_PASSWORD = os.environ.get('ADMIN_DEFAULT_PASSWORD')
-USER_DEFAULT_PASSWORD = os.environ.get('USER_DEFAULT_PASSWORD')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY','xREz9ZXaDs3dX/3t1NLHHVR+0sL8LFr4Ru0s5KnAfMKU6cKZqkBdjwYv8tSKtxZP')
+ADMIN_DEFAULT_PASSWORD = os.environ.get('ADMIN_DEFAULT_PASSWORD','0123')
+USER_DEFAULT_PASSWORD = os.environ.get('USER_DEFAULT_PASSWORD','4567')
 DB_FILE = os.environ.get('DATABASE_FILE','mainsys.db')
 EXCEL_FILE = os.environ.get('EXCEL_FILE', 'mainsys.xlsx')
 TABLE_NAME = 'contracts'
